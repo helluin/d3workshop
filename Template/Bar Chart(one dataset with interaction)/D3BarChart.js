145e5f4c4c79b8189c2cb5a93e6764b419ea7ced
yz1184@nyu.edu <yz1184@nyu.edu>
2016-01-21 16:40:32 -0500

    updated file

--- Template/Bar Chart(one dataset with interaction)/D3BarChart.js
+++ Template/Bar Chart(one dataset with interaction)/D3BarChart.js
@@ -5,11 +5,6 @@ h = 600;
 var dataset = [5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
        11, 12, 15, 20, 18, 29, 16, 18, 23, 25];
 
-var mySVG = d3.select("body")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
-
 
 
 
