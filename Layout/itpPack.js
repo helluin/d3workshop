f9db84caafc7c8b58f78e05f33673f24e95457bc
yz1184@nyu.edu <yz1184@nyu.edu>
2016-01-21 16:49:34 -0500

    updated layout example

--- Layout/itpPack.js
+++ Layout/itpPack.js
@@ -22,7 +22,6 @@ console.log(myPack);
 //2.draw the shapes using the generated data
 d3.json("itpProfessors.json", function (data) {
     var nodes = myPack.nodes(data);
-    console.log(data)
     console.log(nodes);
 
     var node = mySVG.selectAll("circle")
